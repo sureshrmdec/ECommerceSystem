236849309d39716907b4579857e04119b3b55613
Johnson Li <bowenli86@gmail.com>
2013-03-27 17:10:21 +0800

    Web UI

---
+++ src/webController/ListProductServlet.java
@@ -0,0 +1,31 @@
+package webController;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.BusinessService;
+
+public class ListProductServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		BusinessService service = new BusinessService();
+		Map map = service.getAllComputer();
+		request.setAttribute("map",map);
+		
+		request.getRequestDispatcher("/WEB-INF/jsp/listProduct.jsp").forward(request,response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
